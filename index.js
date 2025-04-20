9ec6b5579029ca84dcda6646c2b95fd668bc2fbf
SiddharthaArepalli <asiddharthagoud@gmail.com>
2025-04-20 14:56:46 +0530

    made some changes

--- index.js
+++ index.js
@@ -4,7 +4,22 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:5173" }));
+const allowedOrigins = [
+    "http://localhost:5173",
+    "https://pay-karo-frontend-navy.vercel.app/"
+  ];
+
+  app.use(cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true
+  }));
+
 app.use(express.json());
 app.use('/api/v1', rootRouter);
 
